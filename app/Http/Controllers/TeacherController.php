cd2bd6aa059c73f6d5fa2f416ad4b20c20c1b01d
sjardon <santiagohernanjardon@gmail.com>
2020-10-07 08:45:46 -0300

    Agrega funcionalidad a TeacherController

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -14,7 +14,17 @@ class TeacherController extends Controller
      */
     public function index()
     {
-        //
+
+      $teachers = Teacher::all();
+
+      $teachers->map(function($teacher){
+        $teacher->user();
+        $teacher->image();
+
+        return $teacher;
+      });
+
+      return response(['teachers'=>$teachers]);
     }
 
     /**
@@ -25,7 +35,42 @@ class TeacherController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+      if(!$request->user()->hasAnyRole(['admin','editor'])){
+        return response('Unauthorized',401);
+      }
+
+      $validator = Validator::make($request->all(), [
+        'name' => 'required|unique:courses|max:255',
+        'surname' => 'required|unique:courses|max:255',
+        'image.id' => 'required|exists:images,id',
+        'user.id' => 'required|exists:user,id',
+        'cv' => 'max:40000',
+        'biography' => 'max:40000',
+        'webUrl' => 'max:150'
+        ])->validate();
+
+        $teacher = new Teacher([
+          'name' => $request->input('name'),
+          'surname' => $request->input('surname'),
+          'cv' => $request->input('cv'),
+          'biography' => $request->input('biography'),
+          'webUrl' => $request->input('webUrl')
+        ]);
+
+        $i = Image::find($request->input('image.id'));
+        $u = User::find($request->input('user.id'));
+
+        $teacher->image()->associate($i);
+        $teacher->user()->associate($u);
+
+        $teacher->save();
+
+        if(!$teacher){
+          return response('Error de creación',500);
+        }
+
+        return response(["teacher"=>$teacher]);
     }
 
     /**
@@ -36,7 +81,10 @@ class TeacherController extends Controller
      */
     public function show(Teacher $teacher)
     {
-        //
+      $teacher->user();
+      $teacher->image();
+
+      return response(['teacher'=>$teacher]);
     }
 
     /**
@@ -48,7 +96,52 @@ class TeacherController extends Controller
      */
     public function update(Request $request, Teacher $teacher)
     {
-        //
+
+      $validator = Validator::make($request->all(), [
+        'name' => 'required|unique:courses|max:255',
+        'surname' => 'required|unique:courses|max:255',
+        'image.id' => 'required|exists:images,id',
+        'user.id' => 'required|exists:user,id',
+        'cv' => 'max:40000',
+        'biography' => 'max:40000',
+        'webUrl' => 'max:150'
+        ])->validate();
+
+
+        if(!$request->user()->hasAnyRole(['admin','editor'])){
+          if($request->user()->id != $request->input('user.id')){
+            return response('Unauthorized',401);
+          }
+        }
+
+        $teacher = new Teacher([
+          'name' => $request->input('name'),
+          'surname' => $request->input('surname'),
+          'cv' => $request->input('cv'),
+          'biography' => $request->input('biography'),
+          'webUrl' => $request->input('webUrl')
+        ]);
+
+
+        $teacher->name = $request->input('name');
+        $teacher->surname = $request->input('surname');
+        $teacher->cv = $request->input('cv');
+        $teacher->biography = $request->input('biography');
+        $teacher->webUrl = $request->input('webUrl');
+
+        $i = Image::find($request->input('image.id'));
+        $u = User::find($request->input('user.id'));
+
+        $teacher->image()->associate($i);
+        $teacher->user()->associate($u);
+
+        $teacher->save();
+
+        if(!$teacher){
+          return response('Error de creación',500);
+        }
+
+        return response(["teacher"=>$teacher]);
     }
 
     /**
@@ -59,6 +152,12 @@ class TeacherController extends Controller
      */
     public function destroy(Teacher $teacher)
     {
-        //
+
+      if(!$request->user()->hasAnyRole(['admin','editor'])){
+        return response('Unauthorized',401);
+      }
+
+      $student->delete();
+      return request('ok');
     }
 }
